fdae1983504f230fd368cb0a90ef8086d34526a5
Artur-Cortez <souza.cortez.013@gmail.com>
2023-06-14 18:02:16 +0000

    atualização da solução do problema 1087

--- 2 - Ad-hoc/1087 (Queen).py
+++ 2 - Ad-hoc/1087 (Queen).py
@@ -1,12 +1,13 @@
 while True:
   x1, y1, x2, y2 = map(int, input().split())
-  if x1 + y1 + x2 + y2 == 0: break
-
-  if x1 == x2 and y1 == y2:
-    print(0)
-
-  elif x1 == x2 or y1 == y2 or x2 - x1 == y2 - y1:
-    print(1)
-
+  movimentos = 0
+  if x1 + x2 + y1 + y2 == 0: break
+  # caso linha reta
+  elif x1 == x2 and y1 == y2: movimentos = 0
+  elif x1 == x2 or y1 == y2:
+    movimentos = 1       
+  elif x1 - x2 == y1 - y2 or x2 - x1 == y2 - y1 or x1 - x2 == y2 - y1 or x2 - x1 == y1 - y2:
+    movimentos = 1
   else:
-    print(2)
+    movimentos = 2
+  print(movimentos)
